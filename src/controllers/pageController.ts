8e84ae48b3cea11661c6ad38df40c4d6ba322d77
thiessen2003 <thiessengabriel@gmail.com>
2022-07-14 16:41:02 -0300

    update

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,42 @@
+import { create } from 'domain';
 import {Request, Response} from 'express';
+
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
 
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (req: Request, res: Response) => {
-
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg',
+        }
+    });
 }
 export const cats = (req: Request, res: Response) => {
-
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg',
+        }
+    });
 }
 export const fishes = (req: Request, res: Response) => {
-
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg',
+        }
+    });
 }
